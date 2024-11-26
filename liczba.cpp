67338de74d57b12b16f28d8ee2119032748775fd
atticuris <326338@stud.umk.pl>
2024-11-26 21:08:04 +0100

    Changed liczba.cpp to check whether it is squarefree

--- liczba.cpp
+++ liczba.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool czy_bezkwadratowa(int liczba) {
+    for (int x = 2; x * x <= liczba; ++x) {
+        if (liczba % (x * x) == 0) {
+        	return false;
+        }
+    }
+    return true;
+}
 
 int main() {
     int liczba;
-    std::cin >> liczba; 
-    std::cout << liczba << std::endl; 
+    cin >> liczba; 
+    cout << liczba << endl; 
+
+    cout << "Liczba: " << liczba << endl;
+    if (bezkwadratowa) {
+        cout << "Jest to liczba bezkwadratowa." << endl;
+    } else {
+        cout << "Nie jest to liczba bezkwadratowa." << endl;
+    }
+
     return 0; 
 }
